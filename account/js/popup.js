abc56be11e2b355d2cfde93037fe7c0b7e6c31cc
CBlandino <christopherblandino0@gmail.com>
2024-04-05 22:14:24 -0400

    updates

--- account/js/popup.js
+++ account/js/popup.js
@@ -1,1 +1,22 @@
-//implement logIn.html , signup.html to be pop up windows
+// popup.js
+
+// Function to show the login popup
+function showLoginPopup() {
+    // Load login popup content dynamically
+    fetch('login-popup.html')
+    .then(response => response.text())
+    .then(html => {
+        // Insert the HTML into the document
+        document.body.insertAdjacentHTML('beforeend', html);
+    })
+    .catch(error => console.error('Error loading login popup:', error));
+}
+
+// Function to close the login popup
+function closeLoginPopup() {
+    // Hide the login popup
+    document.getElementById('loginPopup').remove();
+}
+
+// Add event listener to the login link
+document.getElementById('loginLink').addEventListener('click', showLoginPopup);
